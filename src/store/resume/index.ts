38c2bece947392ccff56fbbaf2c149fdf4dd23c1
Mahdi Mehrjoo <mehdimehr2020@gmail.com>
2023-04-10 16:50:14 +0330

    up ++

--- src/store/resume/index.ts
+++ src/store/resume/index.ts
@@ -9,8 +9,8 @@ export const createResume: any = createAsyncThunk('createResume', async (data: R
   try {
     let resumeLogo = popObjectItemByKey(data, 'avatar')
 
-    data.work_city = '6433ca19792cf4bad82360be'
-    data.residence_city = '6433ca19792cf4bad82360be'
+    data.work_city = '643402d32a0d82fd4b1a4273'
+    data.residence_city = '643402d32a0d82fd4b1a4273'
 
     const response = await ApiRequest.builder().auth().request('post', 'resumes', data)
 
