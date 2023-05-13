97a2de62cdbf449c8c1a98ca0160bf428800edd9
Mahdi Mehrjoo <mehdimehr2020@gmail.com>
2023-05-13 17:30:11 +0330

    feat: notifications popup and view all tab in profile added

---
+++ src/views/pages/user-profile/NotificationsTab.tsx
@@ -0,0 +1,101 @@
+import { Box, Card, Grid, Tab, Theme, Typography, useMediaQuery } from '@mui/material'
+import { notifications } from 'src/layouts/components/vertical/AppBarContent'
+import Icon from 'src/@core/components/icon'
+import CustomAvatar from 'src/@core/components/mui/avatar'
+import TabContext from '@mui/lab/TabContext'
+import TabList from '@mui/lab/TabList'
+import { useState } from 'react'
+import BootstrapTooltip from 'src/@core/components/bootstrap-tooltip'
+
+const NotificationsTab = () => {
+  const [activeTab, setActiveTab] = useState<string>('new')
+
+  // ** Hooks
+  const hideText = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'))
+
+  const handleChange = (event: any, value: string) => {
+    setActiveTab(value)
+  }
+  return (
+    <Grid
+      container
+      sx={{
+        p: '5px 15px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+      spacing={2}
+    >
+      <TabContext value={activeTab}>
+        <div style={{ padding: '0', marginBottom: '20px', width: '100%' }}>
+          <TabList
+            variant='scrollable'
+            scrollButtons='auto'
+            onChange={handleChange}
+            aria-label='customized tabs example'
+            centered
+            sx={{
+              width: '100%',
+              '& .MuiTabs-flexContainer': {
+                display: 'flex',
+                justifyContent: 'center'
+              }
+            }}
+          >
+            <Tab
+              value='new'
+              label={
+                <Box sx={{ display: 'flex', alignItems: 'center', ...(!hideText && { '& svg': { mr: 2 } }) }}>
+                  <Icon icon='ic:outline-mark-chat-unread' />
+                  {!hideText && 'new'}
+                </Box>
+              }
+            />
+            <Tab
+              value='read'
+              label={
+                <Box sx={{ display: 'flex', alignItems: 'center', ...(!hideText && { '& svg': { mr: 2 } }) }}>
+                  <Icon icon='ic:outline-mark-chat-read' />
+                  {!hideText && 'read'}
+                </Box>
+              }
+            />
+          </TabList>
+        </div>
+      </TabContext>
+      {notifications.map((notification: any, index: number) => (
+        <Grid key={`notification-${index}`} item sx={{ width: '80%' }}>
+          <BootstrapTooltip placement='top' title='Tap To Read'>
+            <Card
+              sx={{
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                p: 5,
+                cursor: 'pointer',
+                '&:hover': {
+                  boxShadow: '0px 2px 10px 5px rgb(76 78 100 / 22%)'
+                }
+              }}
+            >
+              {/* <RenderAvatar notification={notification} /> */}
+              <CustomAvatar color='primary' skin='light' alt='Notification Icon' sx={{ mr: 3, width: 40, height: 40 }}>
+                <Icon icon='ion:notifcations' fontSize={30} />
+              </CustomAvatar>
+              <Box sx={{ mx: 4, flex: '1 1', display: 'flex', overflow: 'hidden', flexDirection: 'column' }}>
+                <Typography fontWeight={600}>{notification.title}</Typography>
+                <Typography variant='body2'>{notification.subtitle}</Typography>
+              </Box>
+              <Typography variant='caption' sx={{ color: 'text.disabled' }}>
+                {notification.meta}
+              </Typography>
+            </Card>
+          </BootstrapTooltip>
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+export default NotificationsTab
