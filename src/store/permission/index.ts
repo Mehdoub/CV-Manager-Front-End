dc810f1ec3a6b8f28a47e606a9c934a21367ed2b
Mahdi Mehrjoo <mehdimehr2020@gmail.com>
2023-05-21 11:02:04 +0330

    feat: permissions fixed

--- src/store/permission/index.ts
+++ src/store/permission/index.ts
@@ -3,12 +3,12 @@ import ApiRequest from "src/helpers/ApiRequest";
 import { createExtraReducers, sliceInitialStateWithData } from "src/helpers/functions";
 
 
-export const getPermissionsGrouped : any = createAsyncThunk('getPermissionsGrouped', async (_, {rejectWithValue}) => {
+export const getPermissionsGrouped: any = createAsyncThunk('getPermissionsGrouped', async (_, { rejectWithValue }) => {
   try {
-    const response = await ApiRequest.builder().auth().request('get', 'permissions/entities/grouped')
+    const response = await ApiRequest.builder().auth().request('get', 'permissions/entities/grouped/')
 
     return response
-  } catch (err:any) {
+  } catch (err: any) {
     return rejectWithValue(err?.response)
   }
 })
